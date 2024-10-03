c813a1373da8d016f3b4c21774120b70292ad261
Joel Bech <122796634+JoBch@users.noreply.github.com>
2024-10-03 12:21:59 +0200

    Add files via upload
    Database

---
+++ userdata.sql
@@ -0,0 +1,44 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE TABLE `userdata` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `age` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `newsletter` enum('true','false') NOT NULL DEFAULT 'false',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+INSERT INTO `userdata` (`id`, `name`, `age`, `email`, `phone`, `password`, `newsletter`) VALUES
+(4, 'sss', '444', 'ss', '44', 'a871c47a7f48a12b38a994e48a9659fab5d6376f3dbce37559bcb617efe8662d', 'true');
+INSERT INTO `userdata` (`id`, `name`, `age`, `email`, `phone`, `password`, `newsletter`) VALUES
+(26, 'Anders Svensson', '35', 'anders.svensson@email.se', '2345678901', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'false');
+INSERT INTO `userdata` (`id`, `name`, `age`, `email`, `phone`, `password`, `newsletter`) VALUES
+(27, 'Karin Olsson', '29', 'karin.olsson@email.se', '3456789012', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'false');
+INSERT INTO `userdata` (`id`, `name`, `age`, `email`, `phone`, `password`, `newsletter`) VALUES
+(28, 'Johan Eriksson', '42', 'johan.eriksson@email.se', '4567890123', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'false'),
+(29, 'Anna Larsson', '31', 'anna.larsson@email.se', '5678901234', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'false'),
+(30, 'Peter Jonsson', '27', 'peter.jonsson@email.se', '6789012345', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'false'),
+(31, 'Maria Persson', '38', 'maria.persson@email.se', '7890123456', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'false'),
+(32, 'Filip Gustafsson', '45', 'filip.gustafsson@email.se', '8901234567', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'false'),
+(33, 'Sofia Karlsson', '34', 'sofia.karlsson@email.se', '9012345678', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'false'),
+(34, 'Oskar Lindström', '40', 'oskar.lindstrom@email.se', '0123456789', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'false'),
+(35, 'Hasse', '45', 'hasse@hasse.se', '454545', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'false');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
